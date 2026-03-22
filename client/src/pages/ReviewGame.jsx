46b5303d1ed6ad30673f9b4234f39008ee7e849a
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2026-03-22 16:32:25 -0400

    add ReviewGame minial setup

--- client/src/pages/ReviewGame.jsx
+++ client/src/pages/ReviewGame.jsx
@@ -1,3 +1,12 @@
-export default function ReviewGame(){
-    return <div className="feed">Review Game</div>;
+import { Chessboard } from "react-chessboard";
+
+export default function ReviewGame() {
+  return (
+    <>
+      <div className="feed">
+        <div>Review Game</div>
+        <Chessboard position={"start"} />
+      </div>
+    </>
+  );
 }
