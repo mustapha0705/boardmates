2d30442c1b381a4c04b1cc1830771a5762c7e5a9
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2026-04-22 01:35:58 -0400

    fix server board refresh error

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
+    log: process.env.PRISMA_LOG_QUERIES === 'true' ? ['query', 'error'] : ['error'],
 });
 
 const connectDB = async () => {
